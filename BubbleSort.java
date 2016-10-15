fd0e241b7d677d60051de9f9bdf5beca56f00b76
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2016-10-15 15:15:28 -0700

    Initialising variables

---
+++ BubbleSort.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class BubbleSort{
+
+	public static void main(String[] args){
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int n = reader.nextInt();
+		System.out.println("The number entered was " + n);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Algorithms
+
+Implementation of various algorithms in Java
+1. Bubble Sort
