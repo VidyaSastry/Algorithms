67c65cd73280e77251fdb68c8ad6dbbf3006b811
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2016-10-15 17:29:24 -0700

    Insertion Sort initialization

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class InsertionSort{
+
+	public static void main(String[] args){
+		InsertionSort sort = new InsertionSort();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int count = s.nextInt();
+		s.nextLine();
+		System.out.println("Enter the numbers");
+
+		int [] numbers = new int[count];
+		Scanner numScanner = new Scanner(s.nextLine());
+		for (int i = 0; i < count; i++) {
+			if (numScanner.hasNextInt()) {
+				numbers[i] = numScanner.nextInt();
+			}
+		}
+
+		System.out.println("Select\n1 - Ascending Order \n2 - Descending Order");
+		int choice = s.nextInt();
+
+		switch(choice){
+			case 1: sort.ascending(numbers,count);
+			break;
+			case 2: sort.descending(numbers,count);
+			break;
+			default: System.out.println("Invalid choice");
+			break;
+		}
+	}
+
+	public void ascending(int numbers[], int count){
+
+	}
+
+	public void descending(int numbers[], int count){
+		
+	}
+}
