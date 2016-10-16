68e9c56626da77b1f1ec717fbefdbe6cf92a01af
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2016-10-16 11:39:59 -0700

    Heap Sort Initialization

---
+++ Sorting/HeapSort.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class HeapSort{
+
+	public static void main(String[] args){
+		HeapSort sort = new HeapSort();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int count = s.nextInt();
+		s.nextLine();
+		System.out.println("Enter the numbers");
+
+		numbers = new int[count];
+		Scanner numScanner = new Scanner(s.nextLine());
+		for (int i = 0; i < count; i++) {
+			if (numScanner.hasNextInt()) {
+				numbers[i] = numScanner.nextInt();
+			}
+		}
+
+		System.out.println("Select\n1 - Ascending Order \n2 - Descending Order");
+		int choice = s.nextInt();
+	}
+}
