8c13ba87f237f08dd76c07da07981533a3ccac9a
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2016-10-15 17:06:11 -0700

    Selection Sort asc/desc

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class SelectionSort{
+	public static void main(String[] args){
 	SelectionSort sort = new SelectionSort();
 	Scanner s = new Scanner(System.in);
 		System.out.println("Enter the number of elements");
@@ -28,4 +29,49 @@ class SelectionSort{
 			default: System.out.println("Invalid choice");
 			break;
 		}
+	}
+
+	public void ascending(int numbers[],int count){
+
+		int temp;
+		for(int i = 0; i < count; i++){
+			int min = i;
+			for(int j = i; j < count; j++){
+				if(numbers[j] < numbers[min])
+					min = j;
+			}
+
+			if(min != i){
+				temp = numbers[i];
+				numbers[i] = numbers[min];
+				numbers[min] = temp;
+			}
+		}
+		System.out.println("The ordered numbers are: ");
+		for (int i = 0; i < count; i++){
+			System.out.println(numbers[i]);
+		}
+	}
+
+	public void descending(int numbers[],int count){
+		int temp;
+		for(int i = 0; i < count; i++){
+			int max = i;
+			for(int j = i; j < count; j++){
+				if(numbers[j] > numbers[max])
+					max = j;
+			}
+
+			if(max != i){
+				temp = numbers[i];
+				numbers[i] = numbers[max];
+				numbers[max] = temp;
+			}
+		}
+		System.out.println("The ordered numbers are: ");
+		for (int i = 0; i < count; i++){
+			System.out.println(numbers[i]);
+		}
+		
+	}
 }
